8d6c035f1de3305124b6819edf36160da72f604f
OnurHG <onurhangumus@gmail.com>
2022-11-18 09:39:38 +0300

    -Combo initialized

--- Assets/Scripts/Controllers/UI/LevelPanelController.cs
+++ Assets/Scripts/Controllers/UI/LevelPanelController.cs
@@ -18,6 +18,7 @@ public class LevelPanelController : MonoBehaviour
     #endregion
     #region Private Variables
     private int _comboCounter = 1;
+    private int _comboIndeks = 0;
 
     #endregion
     #region Properties
@@ -56,12 +57,43 @@ public class LevelPanelController : MonoBehaviour
             }
 
             ScoreSignals.Instance.onScoreIncrease?.Invoke(ScoreTypeEnums.Score, _comboCounter);
+            UpdateIncreaseText(_comboCounter);
+            UpdateComboCounterText(true);
         }
         else
         {
             _comboCounter = 1;
+            _comboIndeks = 0;
             ScoreSignals.Instance.onScoreIncrease?.Invoke(ScoreTypeEnums.Score, 1);
+            UpdateIncreaseText(1);
+            UpdateComboCounterText(false);
 
         }
+    }
+
+    private void UpdateIncreaseText(int value)
+    {
+        increasedScoreText.alpha = 1;
+
+        increasedScoreText.transform.DOMoveY(1734, 0.5f).SetEase(Ease.InOutBack).OnComplete(()=>
+{
+    increasedScoreText.transform.position = new Vector2(540, 1619);
+    increasedScoreText.alpha = 0;
+    });;
+        increasedScoreText.text = "+" + value;
+    }
+
+    private void UpdateComboCounterText(bool isCombo)
+    {
+        if (isCombo)
+        {
+            ++_comboIndeks;
+            comboCounterText.text = "Perfect x" + _comboIndeks;
+        }
+        else
+        {
+            comboCounterText.text = "";
+        }
+
     }
 }
